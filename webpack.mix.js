75b772c89bdba3040e0cc71d09aa29702592e37b
pie <piestion@gmail.com>
2022-03-02 18:01:18 +0800

    解决静态文件浏览器缓存问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
+/**
+ * version() 函数的调用，解决浏览器静态文件缓存问题
+ * 注意：webpack.mix.js 文件只在 npm run watch-poll 指令执行时引入，之后指令后台运行不再重新引入。
+ * 如果你后台运行 watch-poll 命令的话，需关闭 watch-poll 服务（Ctrl + C），再次启动（ npm run watch-poll ）即可生效。
+ * 参考：https://learnku.com/courses/laravel-intermediate-training/8.x/css-versioning/9903
+ */
+
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+  .sass('resources/sass/app.scss', 'public/css').version()
+  .sourceMaps();
