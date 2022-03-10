2285409de87b25788c30f311566a1086feb9f3b8
pie <piestion@gmail.com>
2022-03-11 01:34:49 +0800

    裁剪图片：添加扩展包包‘‘包包 intervention/image 来处理图片裁剪

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -3,6 +3,7 @@
 namespace App\Handlers;
 
 use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 
 class ImageUploadHandler
@@ -18,7 +19,7 @@ class ImageUploadHandler
      * @param [string] $file_prefix  文件前缀
      * @return 'false'|array
      */
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
         // 构建存储的文件夹规则，值如：uploads/images/avatars/202203/10/
         // 文件切割能让查找效率更高。
@@ -26,7 +27,7 @@ class ImageUploadHandler
 
         // 文件具体存储的物理路径， `public_path()` 获取的是 `public` 文件夹的物理路径。
         // 值如：/mnt/d/docker_env/larabbs/public/uploads/images/avatars/202203/10/
-        $upload_patch = public_path() . '/' . $folder_name;
+        $upload_path = public_path() . '/' . $folder_name;
 
         // 获取文件的后缀名，因图片从剪贴板里黏贴时后缀为空，所以此处确保后缀一直存在
         $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
@@ -41,10 +42,34 @@ class ImageUploadHandler
         }
 
         // 将图片移动到我们的目标存储路径中
-        $file->move($upload_patch, $filename);
+        $file->move($upload_path, $filename);
+
+        // 如果限制了图片宽度， 就进行裁剪
+        if ($max_width && $extension != 'gif') {
+            // 此类中封装的函数，用于裁剪图片
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
 
         return [
             'path' => config('app.url') . '/' . $folder_name . '/' . $filename,
         ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        // 先实例化，传参是文件的磁盘物理路径
+        $image = Image::make($file_path);
+
+        // 进行大小调整的操作
+        $image->resize($max_width, null, function ($constraint) {
+            // 设定宽度是 $max_width，高度等比例缩放
+            $constraint->aspectRatio();
+
+            // 防止截图时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        // 对图片修改后进行保存
+        $image->save();
     }
 }
