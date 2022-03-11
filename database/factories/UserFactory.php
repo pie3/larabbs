411767f4240f47a3df417e01fa68d05b05f79685
pie <piestion@gmail.com>
2022-03-12 01:05:30 +0800

    填充用户表和话题表数据

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,11 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
+    protected $model = User::class;
     /**
      * Define the model's default state.
      *
@@ -14,12 +16,23 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $avatars = [
+            'http://larabbs.test/uploads/images/avatars/202203/11/1_1646933207_Jyy4b58o2n.jpg',
+            'http://larabbs.test/uploads/images/avatars/202203/11/1_1646928598_H6OVxWNkKw.gif',
+            'http://larabbs.test/uploads/images/avatars/202203/11/1_1647012422_csYZ3jiDus.jpg',
+            'http://larabbs.test/uploads/images/avatars/202203/11/1_1647012449_qRBxkplfpD.jpg',
+            'http://larabbs.test/uploads/images/avatars/202203/11/1_1647012470_EPFgSKTf8P.jpg',
+            'http://larabbs.test/uploads/images/avatars/202203/11/1_1647012792_i0aAecAv9r.jpg',
+        ];
+
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$MhrW78PsUaT8XMIqMRiy2elifGC6wc1D0CWzFv6n2FYp4T6A2sMcK', // pie2####3
             'remember_token' => Str::random(10),
+            'introduction' => $this->faker->sentence(),
+            'avatar' => $this->faker->randomElement($avatars),
         ];
     }
 
