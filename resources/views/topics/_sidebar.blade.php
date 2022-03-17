62fa42de8d4d0f926905a89b5a70185363b09eb2
pie <piestion@gmail.com>
2022-03-17 23:17:14 +0800

    用户可以创建话题，并限制只有登录用户可以创建

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,5 +1,7 @@
 <div class="card">
-    <div class="card-body">
-        右边导航栏
-    </div>
+  <div class="card-body">
+    <a class="btn btn-success btn-block" aria-label="Left Align" href="{{ route('topics.create') }}">
+      <i class="fas fa-pencil-alt mr-2"></i> 新增帖子
+    </a>
+  </div>
 </div>
