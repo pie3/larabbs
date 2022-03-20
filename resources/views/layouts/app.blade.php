126cf5555365fff3923053e9ff104ebe2f4b0fcf
pie <piestion@gmail.com>
2022-03-20 12:48:17 +0800

    话题内容页面结构、样式调整

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,43 +2,44 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - Laravel BBS</title>
+  <title>@yield('title', 'LaraBBS') - Laravel BBS</title>
+  <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')">
 
-    <!-- Styles -->
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+  <!-- Styles -->
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
-    @yield('styles')
+  @yield('styles')
 
 </head>
 
 <body>
-    <div id="app" class="{{ route_class() }}-page">
+  <div id="app" class="{{ route_class() }}-page">
 
-        @include('layouts._header')
+    @include('layouts._header')
 
-        <div class="container">
+    <div class="container">
 
-            @include('shared._messages')
+      @include('shared._messages')
 
-            @yield('content')
-
-        </div>
-
-        @include('layouts._footer')
+      @yield('content')
 
     </div>
 
-    <!-- Scripts -->
-    <script src="{{ mix('js/app.js') }}"></script>
+    @include('layouts._footer')
 
-    @yield('scripts')
+  </div>
+
+  <!-- Scripts -->
+  <script src="{{ mix('js/app.js') }}"></script>
+
+  @yield('scripts')
 
 </body>
 
