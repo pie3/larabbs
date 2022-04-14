2c81e86a5fd00501b7806efaa0311464a3ebf2c9
pie <piestion@gmail.com>
2022-04-14 18:45:19 +0800

    注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -24,4 +24,12 @@ class UserObserver
     {
         DB::table('topics')->where('user_id', $user->id)->delete();
     }
+
+    public function saving(User $user)
+    {
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'http://larabbs.test/uploads/images/avatars/202203/10/11_1649932084_PLyAIt5wXQ.png';
+        }
+    }
 }
